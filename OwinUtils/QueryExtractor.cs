ce8f05d74759c47e39bce1a3c63c139fc8e7cf68
Robert Byrne <robert@byrnemail.org>
2014-11-13 09:13:23 +0000

    Add extractor for query parameters

---
+++ OwinUtils/QueryExtractor.cs
@@ -0,0 +1,40 @@
+﻿namespace OwinUtils
+{
+    using System;
+    using System.Threading.Tasks;
+    using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+    using EnvDict = System.Collections.Generic.IDictionary<string, object>;
+    using RouteDict = System.Collections.Generic.IDictionary<string, object>;
+    using Owin;
+    using Microsoft.Owin;
+
+    public class QueryExtractor
+    {
+        AppFunc next;
+        string key;
+        private string defaultValue;
+        private string routeKey;
+
+        public QueryExtractor(AppFunc next, string key, string defaultValue)
+        {
+            this.next = next;
+            this.key = key;
+            this.defaultValue = defaultValue;
+            this.routeKey = "RouteParams";
+        }
+
+        public Task Invoke(EnvDict env)
+        {
+            var ctx = new OwinContext(env);
+            var v = ctx.Request.Query.Get(this.key);
+            if (!env.ContainsKey(this.routeKey)) {
+                env[routeKey] = new System.Collections.Generic.Dictionary<string, object>();
+            }
+            var routeDict = (RouteDict) env[this.routeKey];
+            routeDict[this.key] = v ?? this.defaultValue;
+            return next.Invoke(env);
+        }
+
+    }
+}
+
---
+++ OwinUtilsTests/QueryExtractorTests.cs
@@ -0,0 +1,66 @@
+﻿
+namespace OwinUtilsTests
+{
+
+    using Microsoft.Owin;
+    using Microsoft.Owin.Testing;
+    using NUnit.Framework;
+    using Owin;
+    using OwinUtils;
+    using System.Net.Http;
+    using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+    using RouteDict = System.Collections.Generic.Dictionary<string, object>;
+
+
+    public class QueryExtractorTests
+    {
+
+        [Test]
+        public void CanExtractMidleware()
+        {
+            var app = TestServer.Create(builder => {
+                var key = "pq";
+                builder.Use<QueryExtractor>(key, null);
+                AppFunc handler = env =>
+                {
+                    var ctx = new OwinContext(env);
+                    var d = (RouteDict) env["RouteParams"];
+                    return ctx.Response.WriteAsync((string)d[key]);
+                };
+                builder.Run(handler);
+            });
+            var value = "valueOfQueryParameter";
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://xyz.com/blah?pq=" + value);
+            var response = app.HttpClient.SendAsync(request).Result;
+            Assert.IsTrue(response.IsSuccessStatusCode);
+            Assert.AreEqual(value, response.Content.ReadAsStringAsync().Result);
+        }
+
+        [Test]
+        public void UsesDefaultIfNoQuery()
+        {
+            var defaultValue = "a default";
+            var app = TestServer.Create(builder =>
+            {
+                var key = "pq";
+                builder.Use<QueryExtractor>(key, defaultValue);
+                AppFunc handler = env =>
+                {
+                    var ctx = new OwinContext(env);
+                    var d = (RouteDict)env["RouteParams"];
+                    return ctx.Response.WriteAsync((string)d[key]);
+                };
+                builder.Run(handler);
+            });
+            var value = "valueOfQueryParameter";
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://xyz.com/blah?qr=" + value);
+            var response = app.HttpClient.SendAsync(request).Result;
+            Assert.IsTrue(response.IsSuccessStatusCode);
+            Assert.AreEqual(defaultValue, response.Content.ReadAsStringAsync().Result);
+        }
+
+    }
+
+
+}
+
